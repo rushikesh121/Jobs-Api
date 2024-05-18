30fd62289043cce62fb3d24c2d03c73573cb4916
Rushikesh Raut <60637264+rushikesh121@users.noreply.github.com>
2024-05-18 21:43:49 +0530

    Update app.js

--- starter/app.js
+++ starter/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
+const data;
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
