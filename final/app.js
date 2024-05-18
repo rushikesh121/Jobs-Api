da6d583f1e4e94a5569237021a2e760e9461e9e0
Rushikesh Raut <60637264+rushikesh121@users.noreply.github.com>
2024-05-18 21:48:33 +0530

    Update app.js

--- final/app.js
+++ final/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('express-async-errors');
 
 // extra security packages
-const helmet = require('helmet');
+const helmet = require('helmet')
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
